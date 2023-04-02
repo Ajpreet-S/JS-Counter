8e03eaf626a6d99e049c57bd262affd3d55ab5e5
Ajpreet Singh <SINGHAG19@juniata.edu>
2023-04-02 19:55:22 -0400

    feat: tutorial modal completed

--- help.js
+++ help.js
@@ -1,6 +1,18 @@
+// Clicking the help button will now trigger the modal
 const myModal = document.getElementById('helpmodal');
 const myInput = document.getElementById('helpbutton');
-
 myModal.addEventListener('shown.bs.modal', () => {
     myInput.focus()
 })
+
+// Pressing 'Escape' will open up the modal
+document.addEventListener('keydown', (event) => {
+    // Get the pressed key
+    const pressedKey = event.key;
+
+    // If the pressed key is not Escape, don't do anything
+    if (pressedKey !== 'Escape') return;
+
+    // Open the modal by clicking the help button in the navbar
+    myInput.click();
+});
